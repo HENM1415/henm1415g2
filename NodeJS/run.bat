5cf12268eb4e9beb5e016a1a39fd284f4449b495
demiralp <demiralpali@gmail.com>
2014-10-20 16:15:36 +0200

    Updated app to use secure passport.js authentication instead of homebrew.

--- NodeJS/run.bat
+++ NodeJS/run.bat
@@ -5,4 +5,4 @@ start cmd /k mongod --dbpath ./data
 start cmd /k node app.js
 
 :: Launch a chrome window pointing to node app.
-start Chrome --new-window http://127.0.0.1:1337/?type=login&username=lol&password=lolol
+start Chrome --new-window http://127.0.0.1:1337/login
